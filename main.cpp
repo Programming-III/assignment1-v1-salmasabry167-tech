49e861ab304a93f282de1dd2a150f2bfb39baa87
salmasabry167-tech <salmasabry167@gmail.com>
2025-11-12 15:59:32 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,10 +7,26 @@
 #include <iostream>
 using namespace std;
 
+Animal :: public Animal() {
+    name = "";
+    age = 0;
+    isHungry = false;
+}
+Animal :: public Animal(string n, int a,bool hungry) {
+    name = n;
+    age = a;
+    isHungry = hungry;
+}
 
-// ============== MAIN FUNCTION ==============
-int main() {
-    
-    
+ Animal :: ~Animal(){}
+ 
+ void Animal :: display(){
+     cout << name << "," << age << "," << isHungry << endl;
+ }
+
+int main()
+{
+    printf("Hello World");
+
     return 0;
 }
